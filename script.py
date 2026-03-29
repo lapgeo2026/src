c18c620721a2221496f8201f197c83f58b3d2e78
lapgeo2026 <laportegeorges@ymail.com>
2026-03-29 20:51:31 +0200

    Refactor convertPdfToCsv function and remove unused code

--- script.py
+++ script.py
@@ -7,17 +7,18 @@ import pandas as pd
 from jpype import *
 import jpype.imports
 import re
+import csv
 from pathlib import Path
 
 
-def convertPdfToCsv(input, search_strings):
+def convertPdfToCsv(input):
     # Use a breakpoint in the code line below to debug your script.
     assert isinstance(input, object)
     file: str = str(input)
     
     # Path to your PDF file
     pdf_path = input
-    output_path = input.replace(".pdf","")
+    output_path = input.replace(".pdf",".csv")
 
     try:
       
@@ -30,31 +31,36 @@ def convertPdfToCsv(input, search_strings):
     except FileNotFoundError:
       print("Error: PDF file not found.")
       
-    with open(output_path, 'r', encoding='utf-8', errors='ignore') as file:
-      lines = file.readlines()
+    #with open(output_path, 'r', encoding='utf-8', errors='ignore') as file:
+    #  lines = file.readlines()
         
-      regex = re.compile(pattern)
-      matches = []
+    #  regex = re.compile(pattern)
+    #  matches = []
     
-      for line_number, line in enumerate(lines):
-        if re.search(regex, line):
-            matches.append((line_number + 1, line.strip()))
+    #  for line_number, line in enumerate(lines):
+    #    if re.search(regex, line):
+    #        matches.append((line_number + 1, line.strip()))
 
-      if matches:
-        print(f"Found string {pattern} matches in {output_path}:")
+    #  if matches:
+    #    print(f"Found string {pattern} matches in {output_path}:")
         
-        for line_number, line in matches:
-          print(f"- Line {line_number}: {line.strip()}")
-      else:
-          print(f"No matches found in {pdf_path} for the given string {pattern}.")
+    #    for line_number, line in matches:
+    #      print(f"- Line {line_number}: {line.strip()}")
+    #  else:
+    #      print(f"No matches found in {pdf_path} for the given string {pattern}.")
+
+    de=pd.DataFrame(columns=["Date", "DateValeur", "Description", "Debit", "Credit"])
+    df=pd.read_csv(f"{output_path}", sep=',', on_bad_lines='skip', quoting=csv.QUOTE_NONE)
+    de=df.drop(labels=["Débit"], axis=1)
+    print(df)
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     jpype.startJVM()
     
     filePattern = input("pattern to search for filename: ")
-    pattern = input("pattern to search in files: ")
+    #pattern = input("pattern to search in files: ")
     localpath = "C:/Users/Georges/lang/python/"
     localdir=Path(localpath)
     for filename in localdir.rglob("*" +filePattern +"*.pdf"):
-      convertPdfToCsv(str(filename), str(pattern))
+      convertPdfToCsv(str(filename))
